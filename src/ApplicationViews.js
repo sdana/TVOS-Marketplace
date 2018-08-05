9c36a1c109f4f590d411db01bf996a718db10d1e
sdana <seth.dana@gmail.com>
2018-08-05 16:43:29 -0500

    Added more background images

--- src/ApplicationViews.js
+++ src/ApplicationViews.js
@@ -13,7 +13,10 @@ import "./index.css"
 const backgrounds = ["https://images.unsplash.com/photo-1445216978101-0b05a9c8da3d?ixlib=rb-0.3.5&s=028d47aec09c1fae0ac7bbdde206d991&auto=format&fit=crop&w=1591&q=80",
                     "https://images.unsplash.com/photo-1446413145391-40052a2477ee?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=0002cd5cbc5b5d32f8a75fd15471e2b6&auto=format&fit=crop&w=1350&q=80",
                     "https://images.unsplash.com/photo-1464788061904-b026adb5422b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4a6aa5b3608cb4e7f5a6f5d0be6e45e1&auto=format&fit=crop&w=1950&q=80",
-                    "https://images.unsplash.com/photo-1464638681273-0962e9b53566?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ff907dcc91db99272bacb3ac68fe971e&auto=format&fit=crop&w=1950&q=80"]
+                    "https://images.unsplash.com/photo-1464638681273-0962e9b53566?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=ff907dcc91db99272bacb3ac68fe971e&auto=format&fit=crop&w=1950&q=80",
+                    "https://images.unsplash.com/photo-1486754735734-325b5831c3ad?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f3567f2ad7bbcd7d2f18095598ce0245&auto=format&fit=crop&w=1350&q=80",
+                    "https://images.unsplash.com/photo-1503427128716-12b0ed4822bb?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f841787a75346271184e3be94eac765a&auto=format&fit=crop&w=1489&q=80",
+                    "https://images.unsplash.com/photo-1463598208575-2fba20d78efe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4e2aab772a629172e5ee8934cc9b90eb&auto=format&fit=crop&w=1385&q=8"]
 
 const bodyElement = document.querySelector("body")
 
@@ -21,11 +24,11 @@ const bodyElement = document.querySelector("body")
 export default class ApplicationViews extends Component {
     state = {
         // auth: 0
-        timer: true
+        wallpaper: "https://images.unsplash.com/photo-1464788061904-b026adb5422b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4a6aa5b3608cb4e7f5a6f5d0be6e45e1&auto=format&fit=crop&w=1950&q=80"
     }
 
     wallpaperChange = () => {
-        let newURL = backgrounds[Math.floor(Math.random()*3)]
+        let newURL = backgrounds[Math.floor(Math.random()*backgrounds.length)]
         console.log("Background URL", newURL)
         this.setState({wallpaper: newURL})
     }
@@ -42,8 +45,7 @@ export default class ApplicationViews extends Component {
         }
         setInterval(() =>{
             this.wallpaperChange()
-        }, 600000)
-        bodyElement.style.backgroundImage = `${this.state.wallpaper}`
+        }, 60000)
     }
 
     // componentDidUpdate(){
@@ -60,7 +62,7 @@ export default class ApplicationViews extends Component {
         if (this.isAuthenticated()){
             return (
                 <React.Fragment>
-                    <div style={{ width: "100%", height: "100%", backgroundRepeat: "no-repeat", backgroundPosition: "center center", backgroundAttachment: "fixed", backgroundSize: "cover", backgroundImage: `url(${this.state.wallpaper})` }}>
+                    <div style={{ width: "100%", height: "120%", backgroundRepeat: "no-repeat", backgroundPosition: "center center", backgroundAttachment: "fixed", backgroundSize: "cover", backgroundImage: `url(${this.state.wallpaper})` }}>
                     <Route path="/" component={Nav} />
                     <Route exact path="/" component={MainPage} />
                     <Route exact path="/post" render={props => {
