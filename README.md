8756d157305eaeb927b1776870beb7b893d1fc27
sdana <seth.dana@gmail.com>
2018-08-08 14:08:34 -0500

    Changed API endpoint for testing purposes

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# TVOS Marketplace
+TVOS Marketplace
+==================
+An online marketplace emphasised for Tennessee Viticultural and Oenological Society members to post and request items pertaining to winemaking activities.
 
-An online marketplace for Tennessee Viticultural and Oenological Society members to post and request items pertaining to winemaking activities.
+--------------------------------
+###Visit TVOS Marketplace
+TVOS Marketplace is currently deployed at http://tvos-marketplace.s3-website.us-east-2.amazonaws.com/ for testing purposes.
+
+###Testing Locally
+If you would like to test locally:
+    1. git clone this repo
+    2. `npm or yarn install` to pull in all dependencies
+    3. Install json-server globally: `npm i -g json-server`
+    4. Make a copy of Database/db.backup.json and run `json-server -p 5002 -w *your copy of db.backup.json*`
+    5. `npm start` in root directory to start react server
--- src/Components/Api.js
+++ src/Components/Api.js
@@ -1,16 +1,16 @@
 class Api {
     getUsers() {
-        return fetch("http://67.187.101.105:3030/users").then(e => e.json());
+        return fetch("http://localhost:5002/users").then(e => e.json());
     }
     checkUserThing(thing, otherThing) {
-        return fetch(`http://67.187.101.105:3030/users/?${thing}=${otherThing}`).then(e => e.json())
+        return fetch(`http://localhost:5002/users/?${thing}=${otherThing}`).then(e => e.json())
     }
     checkEmail(email) {
-        return fetch(`http://67.187.101.105:3030/users/?email=${email}`).then(e => e.json())
+        return fetch(`http://localhost:5002/users/?email=${email}`).then(e => e.json())
     }
 
     registerUser (name, email, password, region, displayName) {
-        return fetch("http://67.187.101.105:3030/users", {
+        return fetch("http://localhost:5002/users", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -27,7 +27,7 @@ class Api {
 
     editUserInfo (userId, thingToChange, changeValue) {
         console.log(userId, thingToChange, changeValue)
-        return fetch(`http://67.187.101.105:3030/users/${userId}`, {
+        return fetch(`http://localhost:5002/users/${userId}`, {
             method: "PATCH",
             headers: {
                 "Content-Type": "application/json"
@@ -39,7 +39,7 @@ class Api {
     }
     //////////////////////////////////////////////////////////////////////          GENERAL POSTS              /////////////////////////////////////////////
     postItem(userId, title, price, location, category, description, region, photoURL, email, phone){
-        return fetch("http://67.187.101.105:3030/posts", {
+        return fetch("http://localhost:5002/posts", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -59,30 +59,30 @@ class Api {
         }).then(e => e.json());
     }
     getAllPosts(order){
-        return fetch(`http://67.187.101.105:3030/posts?_expand=categorie&_sort=id&_order=${order}`).then(e => e.json())
+        return fetch(`http://localhost:5002/posts?_expand=categorie&_sort=id&_order=${order}`).then(e => e.json())
     }
 
     getPost(postId) {
-        return fetch(`http://67.187.101.105:3030/posts/${postId}`).then(e => e.json())
+        return fetch(`http://localhost:5002/posts/${postId}`).then(e => e.json())
     }
 
     getRegionalPosts(regionId, order){
-        return fetch(`http://67.187.101.105:3030/posts?regionId=${regionId}&_expand=categorie&_sort=id&_order=${order}`).then(e => e.json())
+        return fetch(`http://localhost:5002/posts?regionId=${regionId}&_expand=categorie&_sort=id&_order=${order}`).then(e => e.json())
     }
     ///////////////////////////////////////////////////////////////////          USERS POSTS                //////////////////////////////////////////////////
     getUserPosts(userId){
-        return fetch(`http://67.187.101.105:3030/posts?userId=${userId}&_expand=categorie&_sort=id&_order=desc`).then(e => e.json())
+        return fetch(`http://localhost:5002/posts?userId=${userId}&_expand=categorie&_sort=id&_order=desc`).then(e => e.json())
     }
 
     deleteUserPost(postId){
-        return fetch(`http://67.187.101.105:3030/posts/${postId}`,
+        return fetch(`http://localhost:5002/posts/${postId}`,
             {
             method: "DELETE"
         })
     }
 ///////////////////////////DEPRECATED/////////////////////////////////////
     editPost(postId, title, price, location, category, description) {
-        return fetch(`http://67.187.101.105:3030/posts/${postId}`, {
+        return fetch(`http://localhost:5002/posts/${postId}`, {
             method: "PATCH",
             headers: {
                 "Content-Type": "application/json"
@@ -98,7 +98,7 @@ class Api {
     }
 /////////////////////////////////////////////////////////////////////////
     editItem(postId, editedObject) {
-        return fetch(`http://67.187.101.105:3030/posts/${postId}`, {
+        return fetch(`http://localhost:5002/posts/${postId}`, {
             method: "PATCH",
             headers: {
                 "Content-Type": "application/json"
