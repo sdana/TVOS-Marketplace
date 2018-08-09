747a01a35ee99f3b078794012ddd9adf862c8e45
sdana <seth.dana@gmail.com>
2018-08-09 15:32:27 -0500

    still trying

--- src/Components/Api.js
+++ src/Components/Api.js
@@ -1,16 +1,16 @@
 class Api {
     getUsers() {
-        return fetch("https://localhost/users").then(e => e.json());
+        return fetch("https://localhost:3000/users").then(e => e.json());
     }
     checkUserThing(thing, otherThing) {
-        return fetch(`https://localhost/users/?${thing}=${otherThing}`).then(e => e.json())
+        return fetch(`https://localhost:3000/users/?${thing}=${otherThing}`).then(e => e.json())
     }
     checkEmail(email) {
-        return fetch(`https://localhost/users/?email=${email}`).then(e => e.json())
+        return fetch(`https://localhost:3000/users/?email=${email}`).then(e => e.json())
     }
 
     registerUser (name, email, password, region, displayName) {
-        return fetch("https://localhost/users", {
+        return fetch("https://localhost:3000/users", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -27,7 +27,7 @@ class Api {
 
     editUserInfo (userId, thingToChange, changeValue) {
         console.log(userId, thingToChange, changeValue)
-        return fetch(`https://localhost/users/${userId}`, {
+        return fetch(`https://localhost:3000/users/${userId}`, {
             method: "PATCH",
             headers: {
                 "Content-Type": "application/json"
@@ -39,7 +39,7 @@ class Api {
     }
     //////////////////////////////////////////////////////////////////////          GENERAL POSTS              /////////////////////////////////////////////
     postItem(userId, title, price, location, category, description, region, photoURL, email, phone){
-        return fetch("https://localhost/posts", {
+        return fetch("https://localhost:3000/posts", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -59,30 +59,30 @@ class Api {
         }).then(e => e.json());
     }
     getAllPosts(order){
-        return fetch(`https://localhost/posts?_expand=categorie&_sort=id&_order=${order}`).then(e => e.json())
+        return fetch(`https://localhost:3000/posts?_expand=categorie&_sort=id&_order=${order}`).then(e => e.json())
     }
 
     getPost(postId) {
-        return fetch(`https://localhost/posts/${postId}`).then(e => e.json())
+        return fetch(`https://localhost:3000/posts/${postId}`).then(e => e.json())
     }
 
     getRegionalPosts(regionId, order){
-        return fetch(`https://localhost/posts?regionId=${regionId}&_expand=categorie&_sort=id&_order=${order}`).then(e => e.json())
+        return fetch(`https://localhost:3000/posts?regionId=${regionId}&_expand=categorie&_sort=id&_order=${order}`).then(e => e.json())
     }
     ///////////////////////////////////////////////////////////////////          USERS POSTS                //////////////////////////////////////////////////
     getUserPosts(userId){
-        return fetch(`https://localhost/posts?userId=${userId}&_expand=categorie&_sort=id&_order=desc`).then(e => e.json())
+        return fetch(`https://localhost:3000/posts?userId=${userId}&_expand=categorie&_sort=id&_order=desc`).then(e => e.json())
     }
 
     deleteUserPost(postId){
-        return fetch(`https://localhost/posts/${postId}`,
+        return fetch(`https://localhost:3000/posts/${postId}`,
             {
             method: "DELETE"
         })
     }
 ///////////////////////////DEPRECATED/////////////////////////////////////
     editPost(postId, title, price, location, category, description) {
-        return fetch(`https://localhost/posts/${postId}`, {
+        return fetch(`https://localhost:3000/posts/${postId}`, {
             method: "PATCH",
             headers: {
                 "Content-Type": "application/json"
@@ -98,7 +98,7 @@ class Api {
     }
 /////////////////////////////////////////////////////////////////////////
     editItem(postId, editedObject) {
-        return fetch(`https://localhost/posts/${postId}`, {
+        return fetch(`https://localhost:3000/posts/${postId}`, {
             method: "PATCH",
             headers: {
                 "Content-Type": "application/json"
